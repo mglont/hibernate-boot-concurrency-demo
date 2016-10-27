c1c016868ca244ca260c95aaeeb734be90e2a743
Mihai Glonț <mglont@ebi.ac.uk>
2016-10-27 16:28:52 +0100

    misc changes

--- src/test/groovy/random/domain/UserTests.groovy
+++ src/test/groovy/random/domain/UserTests.groovy
@@ -1,5 +1,6 @@
 package random.domain
 
+
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -27,36 +28,37 @@ class UserTests {
     EntityManagerFactory emf
 
     @Test
-    void testSomething() {
+    void testConcurrentInsertionAndRetrieval() {
         final int ORIG_SIZE = 10; // how many to create in the first session
         final String BAR = "testUsername"
         final AtomicLong idGenerator = new AtomicLong()
 
         EntityManager em1 = emf.createEntityManager()
-
         em1.getTransaction().begin()
-
         try {
-            for (int i = 0; i < ORIG_SIZE; i++) {
+            ORIG_SIZE.times { int i ->
                 User thisUser = new User()
-                thisUser.setUsername(BAR + i)
+                thisUser.setUsername("$BAR${i+1}")
                 thisUser.setId(idGenerator.incrementAndGet())
                 thisUser = userRepository.save(thisUser)
                 em1.flush()
             }
             em1.getTransaction().commit()
         } catch (Exception e) {
-            System.err.println("Cannot insert user from original session")
-            em1.getTransaction().rollback()
+            println("Cannot insert user from original session")
+            if (em1.getTransaction().active) {
+                em1.getTransaction().rollback()
+            }
             throw e
         } finally {
             if (em1?.isOpen()) {
                 em1.close() //end of first session
             }
         }
+        assertFalse("em1 is still open",  em1.isOpen())
 
         // how many new ones to create in different persistence contexts/sessions
-        final int SIZE = 2 * Runtime.getRuntime().availableProcessors()
+        final int SIZE = Runtime.getRuntime().availableProcessors()
         final CountDownLatch startLatch = new CountDownLatch(1)
         final CountDownLatch finishLatch = new CountDownLatch(SIZE)
         final AtomicReferenceArray<User> userRefs = new AtomicReferenceArray<>(SIZE)
@@ -71,6 +73,7 @@ class UserTests {
                     try {
                         startLatch.await()
                     } catch (InterruptedException e) {
+                        println "Worker thread $originalIdx interrupted...: $e"
                         finishLatch.countDown()
                         Thread.currentThread().interrupt()
                     }
@@ -79,24 +82,25 @@ class UserTests {
                     try {
                         thisEm.getTransaction().begin()
                         // creating new content in this session is all right
-                        String newUsername = BAR + newIdx
+                        String newUsername = "$BAR$newIdx"
                         assertNull(userRepository.findByUsername(newUsername))
                         newUser = new User()
                         newUser.setUsername(newUsername)
                         newUser.setId(idGenerator.incrementAndGet())
                         newUser = userRepository.save(newUser)
-                        thisEm.flush()
                         assertNotNull(newUser)
+                        thisEm.flush()
                         thisEm.getTransaction().commit()
+                        userRefs.compareAndSet(originalIdx, null, newUser)
                     } catch(Exception e) {
-                        System.out.println("Rolling back new user $newUser because of $e")
+                        println("Rolling back new user $newUser because of $e")
                         thisEm.getTransaction().rollback()
-                        throw e
+                        thisEm.close()
+                        finishLatch.countDown()
                     }
-                   userRefs.compareAndSet(originalIdx, null, newUser)
 
                     // getting something inserted in another session is problematic
-                    String knownUsername = BAR + originalIdx
+                    String knownUsername = "$BAR$originalIdx"
                     User knownUser = userRepository.findByUsername(knownUsername)
                     try {
                         assertNotNull(knownUser)
@@ -105,6 +109,8 @@ class UserTests {
                         if (thisEm?.isOpen()) {
                             thisEm.close()
                         }
+                        assertFalse "EM for ${Thread.currentThread().name} still open", thisEm.isOpen()
+                        println "${Thread.currentThread().name} -- ${userRepository.count()}"
                         finishLatch.countDown()
                     }
                 }
@@ -112,14 +118,16 @@ class UserTests {
         }
         // simulate throwing concurrent requests to another service
         startLatch.countDown()
+        pool.shutdown()
         try {
             finishLatch.await()
         } catch(InterruptedException e) {
+            println("Interrupted main thread while waiting for worker threads: $e")
             Thread.currentThread().interrupt()
         } finally {
             try {
-                pool.shutdown()
-                pool.awaitTermination(1, TimeUnit.SECONDS)
+                //pool.awaitTermination(1, TimeUnit.SECONDS)
+                assertEquals("some worker threads were still running", 0, finishLatch.count)
                 List<Runnable> queuingJobs = pool.shutdownNow()
                 assertEquals(0, queuingJobs.size())
                 assertTrue(pool.isTerminated())
@@ -129,13 +137,16 @@ class UserTests {
         // see what we got
         EntityManager em2 = emf.createEntityManager()
         ORIG_SIZE.times { int i ->
-            User expected = userRepository.findByUsername(BAR + i)
+            println "from ORIG_SIZE $ORIG_SIZE $i"
+            User expected = userRepository.findByUsername("$BAR${i+1}")
             assertNotNull(expected)
         }
 
+        assertEquals("did not persist all instances", ORIG_SIZE + SIZE, userRepository.count())
         SIZE.times { int i ->
+            println "from SIZE $SIZE $i"
             User actual = userRefs.get(i)
-            User expected = userRepository.findByUsername(BAR + (ORIG_SIZE + 1 + i))
+            User expected = userRepository.findByUsername("$BAR${ORIG_SIZE + 1 + i}")
             assertNotNull(actual)
             assertNotNull(expected)
             assertEquals(expected, actual)
